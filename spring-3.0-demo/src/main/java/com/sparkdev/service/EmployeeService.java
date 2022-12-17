c8e64f2201145e722a35da30efdc34e8688ca82b
Pavan Mantha <manthapavankumar11@gmail.com>
2022-12-17 22:14:57 +0530

    added actuator

--- spring-3.0-demo/src/main/java/com/sparkdev/service/EmployeeService.java
+++ spring-3.0-demo/src/main/java/com/sparkdev/service/EmployeeService.java
@@ -2,6 +2,9 @@ package com.sparkdev.service;
 
 import com.sparkdev.dto.Employee;
 import com.sparkdev.exceptions.NotFoundException;
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -11,17 +14,22 @@ import java.util.List;
 @Service
 public class EmployeeService {
     private List<Employee> employees = new ArrayList<>();
-
+    //Observation Registry feature to publish the metrics
+    @Autowired
+    private ObservationRegistry observationRegistry;
     public Employee save(Employee employee){
         employees.add(employee);
-        return employee;
+        return Observation.createNotStarted("SaveEmployee", observationRegistry).observe(() -> employee);
     }
 
     public List<Employee> getAllEmployees(){
-        return employees;
+        return Observation.createNotStarted("getAllEmployees",observationRegistry).observe(()->employees);
     }
 
     public Employee getEmployeeById(int id)  {
-        return employees.stream().filter(employee -> employee.id() == id).findAny().orElseThrow(() -> new NotFoundException(HttpStatus.NOT_FOUND, "employee with the given id "+id+" not found !"));
+        return Observation.createNotStarted("getEmployeeById",observationRegistry)
+                .observe(() -> employees.stream().filter(employee -> employee.id() == id)
+                        .findAny()
+                        .orElseThrow(() -> new NotFoundException(HttpStatus.NOT_FOUND, "employee with the given id "+id+" not found !")));
     }
 }
